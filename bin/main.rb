227f11c42c72f44f9da12f5deaf73fe6b7b5109a
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-21 17:29:19 +0200

    fix rubocp error

--- bin/main.rb
+++ bin/main.rb
@@ -2,15 +2,16 @@
 
 class TicTacToe
   def initialize
-    @player_1 = ''
-    @player_2 = ''
-    @player_1_played = false
+    @player1 = ''
+    @player2 = ''
+    @player_one_played = false
   end
 
-  def greet
+  def welcome_message
     puts "\t\t\t\t\"TIC TAC TOE GAME\".
     \t\t\t\t\===================
-    It is a game played by 2 individuals. The goal is to make an alignment of selections either vertically, or horizontally or diagonally."
+    It is a game played by 2 individuals. The goal is to make an alignment of selections either vertically,
+    or horizontally or diagonally."
     puts "\n\t\t\t*Here are the rules of the game.*
     \t\t\t---------------------------------
     rule 1: Player 1 will use \"X\" as pattern and player 2 will use \"O\" as pattern.
@@ -20,76 +21,68 @@ class TicTacToe
     puts "\n\t\t\t*Game Board*
     \t\t\t--------------
     "
+  end
 
+  def greet
+    welcome_message
     print_game_board
-
     puts "\n\t\t\t*How to play the game?*
     \t\t\t---------------------------
     1. You need to enter the number whose position you desire to play.
     2. Each player has one turn."
 
-    puts "Do you want to continue? Enter Y if yes otherwise enter any other key"
+    puts 'Do you want to continue? Enter Y if yes otherwise enter any other key'
     input = gets.chomp
-    if continue?(input)
-      players_names
-    else
-      return
-    end
+    return unless continue?(input)
+
+    players_names
   end
 
   def players_names
     puts 'Player 1: Enter your name: '
-    @player_1 = gets.chomp
-    while @player_1 == '' do
+    @player1 = gets.chomp
+    while @player1 == ''
       puts 'Player 1, you did not enter a valid name, please enter your name: '
-      @player_1 = gets.chomp
+      @player1 = gets.chomp
     end
 
     puts 'Player 2: Enter your name: '
-    @player_2 = gets.chomp
-    while @player_2 == '' do
+    @player2 = gets.chomp
+    while @player2 == ''
       puts 'Player 2, you did not enter a valid name, please enter your name: '
-      @player_2 = gets.chomp
+      @player2 = gets.chomp
     end
   end
 
   def print_game_board
-    move_1 = "1"
-    move_2 = "2"
-    move_3 = "3"
-    move_4 = "4"
-    move_5 = "5"
-    move_6 = "6"
-    move_7 = "7"
-    move_8 = "8"
-    move_9 = "9"
-    array = [move_1, move_2, move_3, move_4, move_5, move_6, move_7, move_8, move_9]
+    move1 = '1', move2 = '2', move3 = '3', move4 = '4'
+    move5 = '5', move6 = '6', move7 = '7', move8 = '8'
+    move9 = '9'
+    array = [move1, move2, move3, move4, move5, move6, move7, move8, move9]
 
-    puts "---------------------------"
-    puts "|        |       |        |"
+    puts '---------------------------'
+    puts '|        |       |        |'
     puts "    #{array[0]}    |   #{array[1]}   |   #{array[2]}   "
-    puts "|        |       |        |"
-    puts "  -------+-------+--------"
-    puts "|        |       |        |"
+    puts '|        |       |        |'
+    puts '  -------+-------+--------'
+    puts '|        |       |        |'
     puts "    #{array[3]}    |   #{array[4]}   |   #{array[5]}   "
-    puts "|        |       |        |"
-    puts "  -------+-------+--------"
-    puts "|        |       |        |"
+    puts '|        |       |        |'
+    puts '  -------+-------+--------'
+    puts '|        |       |        |'
     puts "    #{array[6]}    |   #{array[7]}   |   #{array[8]}   "
-    puts "|        |       |        |"
-    puts "---------------------------"
+    puts '|        |       |        |'
+    puts '---------------------------'
   end
 
   def continue?(input)
-    if input.downcase == 'y'
-      return true
-    end
-    return false
+    return true if input.downcase == 'y'
+
+    false
   end
 
   def players_turn
     print_game_board
-    player_1_played? = false
   end
 end
 
