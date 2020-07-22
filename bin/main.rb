7c4a7b14f4ce8ee256d71bb9c7cab44e53f69ac9
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-22 22:37:05 +0200

    Add module which holds all messages

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,8 @@
 #!/usr/bin/env ruby
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/MethodLength
-# rubocop:disable Metrics/PerceivedComplexity
-# rubocop:disable Layout/LineLength
 require './lib/board.rb'
 require './lib/players.rb'
 require './lib/game.rb'
+require './lib/messages.rb'
 
 class TicTacToe
   def initialize
@@ -16,33 +13,25 @@ class TicTacToe
   private
 
   def welcome_message
-    puts "\t\t\t\t\"TIC TAC TOE GAME\".
-    \t\t\t\t\===================
-    It is a game played by 2 individuals. The goal is to make an alignment of selections either vertically,
-    or horizontally or diagonally."
-    puts "\n\t\t\t*Here are the rules of the game.*
-    \t\t\t---------------------------------
-    rule 1: Player 1 will use \"X\" as pattern and player 2 will use \"O\" as pattern.
-    rule 2: A player wins if they make the same pattern vertically, horizontally or diagonally.
-    rule 3: If the board is filled with no win, then that will be a draw.
-    rule 4: A player is not allowed to make the same selection as their counterpart."
+    puts Messages::GAME_INTRODUCTION
+    puts Messages::RULES_OF_THE_GAME
     puts "\n\t\t\t*Game Board*
     \t\t\t--------------
     "
   end
 
   def players_names
-    puts 'Player 1: Enter your name: '
+    puts Messages::PLAYER_ONE_ENTERS_NAME
     @players.player_one = gets.chomp
     while @players.player_one == ''
-      puts 'Player 1, you did not enter a valid name, please enter your name: '
+      puts Messages::PLAYER_ONE_ENTERS_CORRECT_NAME
       @players.player_one = gets.chomp
     end
 
-    puts 'Player 2: Enter your name: '
+    puts Messages::PLAYER_TWO_ENTERS_NAME
     @players.player_two = gets.chomp
     while @players.player_two == ''
-      puts 'Player 2, you did not enter a valid name, please enter your name: '
+      puts Messages::PLAYER_TWO_ENTERS_CORRECT_NAME
       @players.player_two = gets.chomp
     end
   end
@@ -54,12 +43,9 @@ class TicTacToe
 
     board = Board.new
     puts board.print_board
-    puts "\n\t\t\t*How to play the game?*
-    \t\t\t---------------------------
-    1. You need to enter the number whose position you desire to play.
-    2. Each player has one turn."
+    puts Messages::HOW_TO_PLAY
 
-    puts 'Do you want to continue? Enter Y if yes otherwise enter any other key'
+    puts Messages::WANT_CONTINUE
     input = gets.chomp
     return unless Game.continue?(input)
 
@@ -72,7 +58,7 @@ class TicTacToe
       @moves = []
       players_turn
       puts 'You have finished this round.'
-      puts 'Do you wish to continue? Enter Y if yes otherwise enter anyother key.'
+      puts Messages::WANT_CONTINUE
       input = gets.chomp
       interest = Game.continue?(input)
     end
@@ -92,7 +78,7 @@ class TicTacToe
         player_one_input = gets.chomp.to_i
 
         until @players.valid_move?(player_one_input, @moves)
-          puts "\nINVALID INPUT!!, Either Input has already been played or it is not a number.\nEnter a valid number which is between 1 - 9"
+          puts Messages::INVALID_INPUT
           player_one_input = gets.chomp.to_i
         end
         @moves << player_one_input
@@ -108,7 +94,7 @@ class TicTacToe
         player_two_input = gets.chomp.to_i
 
         until @players.valid_move?(player_two_input, @moves)
-          puts "\nINVALID INPUT!!, Either Input has already been played or it is not a number.\nEnter a valid number which is between 1 - 9"
+          puts Messages::INVALID_INPUT
           player_two_input = gets.chomp.to_i
         end
         @moves << player_two_input
@@ -122,15 +108,10 @@ class TicTacToe
       moves_counter += 1
     end
 
-    puts 'This game is a Draw'
+    puts Messages::GAME_IS_A_DRAW
   end
 end
 
 play = TicTacToe.new
 
 play.greet
-
-# rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/MethodLength
-# rubocop:enable Metrics/PerceivedComplexity
-# rubocop:enable Layout/LineLength
