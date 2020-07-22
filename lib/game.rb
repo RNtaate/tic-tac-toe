60ddcb884a64bdb8d0b7de153b9cea4414d010f2
RNtaate <royhyde14@gmail.com>
2020-07-22 17:59:26 +0300

    Add logic to winning_move? method

--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,7 @@ class Game
     @moves = moves
   end
 
-  def self.winning_move?(_moves_array)
+  def self.winning_move?(moves_array)
     # counter = 0
     # while counter is less than @moves.length
     # if winning condition
@@ -12,8 +12,42 @@ class Game
     # increment counter
     # end while
     # false
+    player1 = []
+    player2 = []
     possiblities = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-    true
+
+    i = 0
+    while i < moves_array.length do
+      if (i + 1).odd?
+        player1 << moves_array[i]
+      else
+        player2 << moves_array[i]
+      end
+      i += 1
+    end
+
+    if player1.length >=3
+      j = 0
+      while j < possiblities.length do 
+        if possiblities[j].all?{|num| player1.include? num}
+          return true
+        end
+        j += 1
+      end
+    end
+
+    if player2.length >=3
+      j = 0
+      while j < possiblities.length do 
+        if possiblities[j].all?{|num| player2.include? num}
+          return true
+        end
+        j += 1
+      end
+    end
+
+
+    false
     end
 
   def drawing_move?
