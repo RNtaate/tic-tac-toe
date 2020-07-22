7e20aa24482a378351598cbe6da3b349aba6b629
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-22 21:51:02 +0200

    Finish fix all rubocop errors

--- lib/game.rb
+++ lib/game.rb
@@ -3,21 +3,14 @@ class Game
     @moves = moves
   end
 
+  # rubocop:disable Metrics/CyclomaticComplexity
   def self.winning_move?(moves_array)
     player1 = []
     player2 = []
     possiblities = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
                     [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
-    i = 0
-    while i < moves_array.length
-      if (i + 1).odd?
-        player1 << moves_array[i]
-      else
-        player2 << moves_array[i]
-      end
-      i += 1
-    end
+    posibilities_map(player1, player2, moves_array, possiblities)
 
     if player1.length >= 3
       j = 0
@@ -39,10 +32,23 @@ class Game
 
     false
   end
+  # rubocop:enable Metrics/CyclomaticComplexity
 
   def self.continue?(input)
     return true if input.downcase == 'y'
 
     false
+  end
+
+  def self.posibilities_map(player1, player2, moves_array, _possiblities)
+    i = 0
+    while i < moves_array.length
+      if (i + 1).odd?
+        player1 << moves_array[i]
+      else
+        player2 << moves_array[i]
+      end
+      i += 1
+    end
   end
 end
